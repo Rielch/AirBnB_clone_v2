0edc4ab6659ba8f99fa28043265ed476b8751aa1
Miguel22247 <minipachru@gmail.com>
2021-08-24 13:18:36 -0300

    change variable names

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,8 +9,8 @@ def do_pack():
     """function that generates .tgz file"""
     try:
         local("mkdir -p versions")
-        n = datetime.now()
-        filename = "versions/web_static_" + n.strftime("%Y%m%d%H%M%S") + ".tgz"
+        date = datetime.now()
+        filename = "versions/web_static_" + date.strftime("%Y%m%d%H%M%S") + ".tgz"
         local("tar -cvzf " + filename + " web_static")
         return filename
     except:
