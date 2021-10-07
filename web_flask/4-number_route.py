d9c7d0584bb731ac9180a685360a05e42cc56fbc
Gabriel Prochnau <2288@holbertonschool.com>
2021-10-07 18:16:24 +0000

    Add part 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Starts a Flask application and display Hello HBNB!"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """Displays Hello HBNB!"""
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Displays HBNB"""
+    return("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Displays C followed by the value of text"""
+    return("C {}".format(text.replace('_', ' ')))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Display Python followed by the value of text"""
+    return("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """Display n is a number if n is an integer"""
+    return("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
