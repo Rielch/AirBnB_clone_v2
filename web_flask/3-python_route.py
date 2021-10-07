e572c959454c30732940ad63c21b4c5f0df266c7
Gabriel Prochnau <2288@holbertonschool.com>
2021-10-07 18:11:05 +0000

    Add part 3 and fix part 2

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def c_is_fun(text):
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text="is cool"):
     """Display Python followed by the value of text"""
-    return("Python <text>".format(text.replace('_', ' ')))
+    return("Python {}".format(text.replace('_', ' ')))
 
 
 if __name__ == "__main__":
