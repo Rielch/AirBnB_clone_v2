50323bcebfa30362edb72d563e619afa188546b2
Gabriel Prochnau <2288@holbertonschool.com>
2021-10-07 19:32:25 +0000

    Add part 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def is_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """Displays a HTML template if n is an integer"""
-    return(render_template('5-number.html', num=n))
+    return(render_template('templates/5-number.html', num=n))
 
 
 if __name__ == "__main__":
