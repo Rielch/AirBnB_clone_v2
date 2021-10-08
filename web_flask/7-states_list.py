52d0eaa67bdc60aca97bff2d476dbc1e79cb1a8b
Gabriel Prochnau <2288@holbertonschool.com>
2021-10-08 13:29:01 +0000

    Add part 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Starts a web application and displays a list of states"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
